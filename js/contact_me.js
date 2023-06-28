a1c56eabecccca2700b8b947f165e327063ccf01
kenneth-alt <kenneth.ugo15@gmail.com>
2023-06-28 03:29:35 -0600

    Modify contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,68 +1,69 @@
-$(function() {
-
-    $("input,textarea").jqBootstrapValidation({
-        preventSubmit: true,
-        submitError: function($form, event, errors) {
-            // additional error messages or events
-        },
-        submitSuccess: function($form, event) {
-            event.preventDefault(); // prevent default submit behaviour
-            // get values from FORM
-            var name = $("input#name").val();
-            var email = $("input#email").val();
-            var message = $("textarea#message").val();
-            var firstName = name; // For Success/Failure Message
-            // Check for white space in name for Success/Fail message
-            if (firstName.indexOf(' ') >= 0) {
-                firstName = name.split(' ').slice(0, -1).join(' ');
-            }
-            $.ajax({
-                url: "././mail/contact_me.php",
-                type: "POST",
-                data: {
-                    name: name,
-                    email: email,
-                    message: message
-                },
-                cache: false,
-                success: function() {
-                    // Success message
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success')
-                        .append('</div>');
-
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-                error: function() {
-                    // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-                    $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-            })
-        },
-        filter: function() {
-            return $(this).is(":visible");
-        },
-    });
-
-    $("a[data-toggle=\"tab\"]").click(function(e) {
-        e.preventDefault();
-        $(this).tab("show");
-    });
-});
-
-
-/*When clicking on Full hide fail/success boxes */
-$('#name').focus(function() {
-    $('#success').html('');
-});
+(function () {
+    // Initialize EmailJS with your user ID
+    emailjs.init("_UyzTNpPZikFOjrvP");
+  })();
+  
+  function validateForm() {
+    var nameInput = document.getElementById("name");
+    var emailInput = document.getElementById("email");
+    var messageInput = document.getElementById("message");
+  
+    var nameErrorElement = document.getElementById("name-error");
+    var emailErrorElement = document.getElementById("email-error");
+    var messageErrorElement = document.getElementById("message-error");
+  
+    // Clear previous error messages
+    nameErrorElement.innerHTML = "";
+    emailErrorElement.innerHTML = "";
+    messageErrorElement.innerHTML = "";
+  
+    var isValid = true;
+  
+    // Perform form validation here
+    if (nameInput.value.trim() === "") {
+      nameErrorElement.innerHTML = "Please enter your name";
+      isValid = false;
+    }
+  
+    if (emailInput.value.trim() === "") {
+      emailErrorElement.innerHTML = "Please enter your email";
+      isValid = false;
+    }
+  
+    if (messageInput.value.trim() === "") {
+      messageErrorElement.innerHTML = "Please enter your message";
+      isValid = false;
+    }
+  
+    if (!isValid) {
+      return false; // Prevent form submission
+    }
+  
+    // Form is valid, proceed with email sending
+    sendEmail();
+  
+    // Prevent form submission
+    return false;
+  }
+  
+  function sendEmail() {
+    var params = {
+      from_name: document.getElementById("name").value,
+      email_id: document.getElementById("email").value,
+      message: document.getElementById("message").value,
+    };
+  
+    var responseMessageElement = document.getElementById("response-message");
+    responseMessageElement.innerHTML = ""; // Clear any previous messages
+  
+    emailjs.send("service_4fd7ty9", "template_ikz5lep", params)
+      .then(function (res) {
+        responseMessageElement.innerHTML = "Message successfully sent";
+        responseMessageElement.className = "success-message";
+      })
+      .catch(function (error) {
+        responseMessageElement.innerHTML = "Error sending email: " + error;
+        responseMessageElement.className = "error-message";
+      });
+  }
+  
